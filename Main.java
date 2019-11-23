0c85cb223b0e5ca6168f8e20633a95ce4fa2db36
Ashish Vaish <42945234+ashprog@users.noreply.github.com>
2019-11-23 14:56:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.number;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String...args)
+	{
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter a number (upto lakhs) :");
+		long startTime = System.currentTimeMillis();
+		String s = Number.toWords(sc.nextInt());
+		long totalTime = System.currentTimeMillis()-startTime;
+		System.out.println(s);
+		System.out.println("\nEnd : "+totalTime+" ms");
+	}
+}
